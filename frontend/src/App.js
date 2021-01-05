4c53566034c185a3e6879397b415ac9c8b56d751
daniel <daniel.szarek98@gmail.com>
2021-01-05 22:02:08 +0100

    Add possibility to post new water items

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import MyNavbar from "./components/Home/navbar";
 import React from "react";
 import Home from "./components/Home/home";
 import waterEditItem from "./components/WaterTable/waterEditItem";
+import waterAddItem from "./components/WaterTable/waterAddItem";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Switch>
                     <Route exact path="/" component={Home}/>
                     <Route path="/water/:id" component={waterEditItem}/>
+                    <Route path="/water-add/" component={waterAddItem}/>
                 </Switch>
             </BrowserRouter>
         </div>
